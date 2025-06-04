64433f925799f2f9f3d9b759d54fb166b6c8d1fe
JairPC <Yayireliecer8322@gmail.com>
2025-06-04 16:49:59 -0500

    Includes added

--- Jidi Player/Main.cpp
+++ Jidi Player/Main.cpp
@@ -1,3 +1,4 @@
+#include <ostream>
 #include <windows.h>
 #include <thread>
 
@@ -5,7 +6,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+#pragma comment(lib, "OmniMIDI.lib")
+
 #include <OmniMIDI.h>
-#include <ostream>
+#include "Include/MidiFile.h"
+
+// Graphics libraries (Need ig but. Coming soon)
+//#include <d3d11.h>
+//#include <dxcapi.h>
+//#include <dxcore.h>
+//#include <dxdiag.h>
+//#include <d3dcompiler.h>
 
 using namespace std;
+
+int main() {
+	
+}
