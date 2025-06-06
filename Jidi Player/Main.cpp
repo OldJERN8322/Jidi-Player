83d523bba3b1b792edad51fe9bf09183941ebd4f
JairPC <Yayireliecer8322@gmail.com>
2025-06-06 15:24:57 -0500

    Check build in VS2022 and then what happened

--- Jidi Player/Main.cpp
+++ Jidi Player/Main.cpp
@@ -6,10 +6,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-#pragma comment(lib, "OmniMIDI.lib")
+#pragma comment(lib, "OmniMIDI_Win64.lib")
 
 #include <OmniMIDI.h>
-#include "Include/MidiFile.h"
+#include <MidiFile.h>
 
 // Graphics libraries (Need ig but. Coming soon)
 //#include <d3d11.h>
@@ -20,6 +20,16 @@
 
 using namespace std;
 
-int main() {
-	
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
+	InitializeKDMAPIStream();
+	cout << "Hello World!" << endl;
+	Sleep(400);
+	DWORD noteOn = 0x90 | (60 << 8) | (127 << 16);
+	SendDirectData(noteOn);
+
+	Sleep(500);
+
+	DWORD noteOff = 0x90 | (60 << 8);
+	SendDirectData(noteOff);
+
 }
